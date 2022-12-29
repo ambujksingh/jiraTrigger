b123d3a0ce1c097a04639ab77eeae71add14270e
Ambuj Singh <ambuj.singh@emerson.com>
2022-12-29 17:35:50 +0530

    url changes

--- TimerTriggerJiraTask/index.js
+++ TimerTriggerJiraTask/index.js
@@ -6,7 +6,7 @@ module.exports = async function (context, myTimer) {
         if (myTimer.isPastDue) {
             context.log('JavaScript is running late!');
         }
-        let baseUrl = 'https://ers-dev.azurewebsites.net/api/ersUserScheduler';
+        let baseUrl = 'https://ers-dev.azurewebsites.net/api/jiraTaskScheduler';
         let results = await jiraTask.initiateTrigger(baseUrl);
         context.log(results)
         context.log('JavaScript timer trigger function ran!', timeStamp);
